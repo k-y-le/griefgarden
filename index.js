cd8798e7c5e37290a84d1ee94e85bce119516220
Kyle Barnes <ktbarnes@princeton.edu>
2022-03-29 00:40:43 -0400

    switch to postgres

--- index.js
+++ index.js
@@ -1,59 +1,67 @@
 const express = require('express');
 var router = require('./router');
-var sqlite3 = require('sqlite3');
+// var sqlite3 = require('sqlite3');
 var path = require('path');
 
 var app = express()
 
-
-var db = new sqlite3.Database('./mem.db', sqlite3.OPEN_READWRITE, (err) => {
-
-    if (err && err.code == "SQLITE_CANTOPEN") {
-        createDatabase();
-        return;
-        } else if (err) {
-            console.log("Getting error " + err);
-            exit(1);
-    }
-    runQueries(db);
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-function createDatabase() {
-  console.log("creating db anew");
-    var newdb = new sqlite3.Database('mem.db', (err) => {
-        if (err) {
-            console.log("Getting error " + err);
-            exit(1);
-        }
-        createTables(newdb);
-    });
-}
-
-function createTables(newdb) {
-    newdb.exec(`
-    create table memorial (
-        id int primary key not null,
-        title text not null,
-        author text not null,
-        desc text not null,
-        color text not null,
-        time text not null
-    );
-    ` , ()  => {
-            runQueries(newdb);
-    });
-}
-
-function runQueries(db) {
-    db.all(`
-    select * from memorial`, (err, rows) => {
-        rows.forEach(row => {
-            console.log(row.id + "\t" +
-            row.title + "\t" +
-            row.author);
-        });
-    });
-}
+//
+// var db = new sqlite3.Database('./mem.db', sqlite3.OPEN_READWRITE, (err) => {
+//
+//     if (err && err.code == "SQLITE_CANTOPEN") {
+//         createDatabase();
+//         return;
+//         } else if (err) {
+//             console.log("Getting error " + err);
+//             exit(1);
+//     }
+//     runQueries(db);
+// });
+//
+// function createDatabase() {
+//   console.log("creating db anew");
+//     var newdb = new sqlite3.Database('mem.db', (err) => {
+//         if (err) {
+//             console.log("Getting error " + err);
+//             exit(1);
+//         }
+//         createTables(newdb);
+//     });
+// }
+//
+// function createTables(newdb) {
+//     newdb.exec(`
+//     create table memorial (
+//         id int primary key not null,
+//         title text not null,
+//         author text not null,
+//         desc text not null,
+//         color text not null,
+//         time text not null
+//     );
+//     ` , ()  => {
+//             runQueries(newdb);
+//     });
+// }
+//
+// function runQueries(db) {
+//     db.all(`
+//     select * from memorial`, (err, rows) => {
+//         rows.forEach(row => {
+//             console.log(row.id + "\t" +
+//             row.title + "\t" +
+//             row.author);
+//         });
+//     });
+// }
 
 // Insert
 var bodyParser = require("body-parser");
@@ -73,34 +81,68 @@ var public = path.join(__dirname, 'public');
 
 app.post('/addmem', function(req, res){
   console.log("adding memorial " + req.body.title + " in cell " + req.body.id);
-  var db = new sqlite3.Database('./mem.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-      console.log(err.code);
-      return;
-    }
-    var time = new Date().toString();
-    let insert = 'INSERT INTO memorial (id, title, author, desc, color, time) VALUES (?,?,?,?,?,?)';
-    db.run(insert, [req.body.id, req.body.title, req.body.author, req.body['narrative[]'], req.body.color, time]);
-    runQueries(db);
-  });
+  // connect to db
+  // add memorial
+  try {
+    const client = await pool.connect();
+    const result = await client.query(
+      'INSERT INTO memorial (id, title, author, desc, color, time) VALUES ($1, $2, $3, $4, $5, $6)',
+      [name, email, id],
+      (error, results) => {
+        if (error) {
+          throw error
+        }
+        response.status(200).send(`User modified with ID: ${id}`)
+      }
+    )
+    const results = { 'results': (result) ? result.rows : null};
+    res.send(results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+  // var db = new sqlite3.Database('./mem.db', sqlite3.OPEN_READWRITE, (err) => {
+  //   if (err) {
+  //     console.log(err.code);
+  //     return;
+  //   }
+  //   var time = new Date().toString();
+  //   let insert = 'INSERT INTO memorial (id, title, author, desc, color, time) VALUES (?,?,?,?,?,?)';
+  //   db.run(insert, [req.body.id, req.body.title, req.body.author, req.body['narrative[]'], req.body.color, time]);
+  //   runQueries(db);
+  // });
 });
 
 app.get('/getmem', function(req, res){
-  var db = new sqlite3.Database('./mem.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-      console.log(err.code);
-      return;
-    }
-
-    let sql = `SELECT * FROM memorial`;
-    db.all(sql, [], (err, rows) => {
-      if (err) {
-        console.log(err.code);
-        return;
-      }
-      res.send(rows);
-    });
-  });
+  // connect to db
+  // get all in memorial table
+  // res.send(all)
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM memorial');
+    const results = { 'results': (result) ? result.rows : null};
+    res.send(results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+  // var db = new sqlite3.Database('./mem.db', sqlite3.OPEN_READWRITE, (err) => {
+  //   if (err) {
+  //     console.log(err.code);
+  //     return;
+  //   }
+  //
+  //   let sql = `SELECT * FROM memorial`;
+  //   db.all(sql, [], (err, rows) => {
+  //     if (err) {
+  //       console.log(err.code);
+  //       return;
+  //     }
+  //     res.send(rows);
+  //   });
+  // });
 });
 
 app.use(express.static(__dirname + "/public"));
