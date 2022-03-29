b18d8386e11b708494aa478f772d27d6829f2899
Kyle Barnes <ktbarnes@princeton.edu>
2022-03-29 01:40:01 -0400

    Update grid.js

--- public/assets/js/grid.js
+++ public/assets/js/grid.js
@@ -164,7 +164,7 @@ var generateGrid = new Promise( function(resolve, reject){
     }
     $.get('/getmem', function(data, status){
       data.forEach((mem) => {
-        console.log(mem.id);
+        console.log(mem);
         var newMem = new Memorial(mem.id, mem.title, mem.author, mem.narrative, mem.color, zoneSymbols[cells[mem.id].zone - 1]);
         var zoneCol = zoneColors[cells[mem.id].zone - 1];
         cells[mem.id].zone = 7;
